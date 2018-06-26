31bcb342b63b630a8cec521291413fa0781947b4
spikir <spikirard@gmail.com>
2018-06-26 13:57:55 +0200

    V. 1.1.0

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
 	var array = [];
 	var slidearr = [];
-	var slidePos = "";
 	var currentIndex1 = '';
 	var currentIndex2 = '';
 	var slidePos = '';
 	
 	$(window).resize(function() {
+		$('#particles-js').css('height', $(window).innerHeight());
+	});
+	
+	$(window).on('load',function(){
 		$('#particles-js').css('height', $(window).innerHeight());
 	});
 	
